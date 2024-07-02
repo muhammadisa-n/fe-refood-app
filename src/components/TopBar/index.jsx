b90a88786d158415d5512a2862c97f88329a2062
muhammadisa226 <muhammadisa226@gmail.com>
2024-07-03 06:25:02 +0700

    almost finish

--- src/components/TopBar/index.jsx
+++ src/components/TopBar/index.jsx
@@ -1,11 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import defaultProfileImg from '@assets/userdefault.png'
 import { useUser } from '@context/userContext.jsx'
 import { RiMenu2Line } from 'react-icons/ri'
+import { jwtDecode } from 'jwt-decode'
+import { FaWhatsapp } from 'react-icons/fa'
 const TopBar = ({ toggleSidebar }) => {
     const { user, refreshUser } = useUser()
+    const [token, setToken] = useState(localStorage.getItem('access_token'))
+    const decoded = jwtDecode(token)
     useEffect(() => {
-        refreshUser()
+        if (token) {
+            refreshUser()
+        }
     }, [])
     return (
         <div className='flex  items-center justify-between md:justify-end h-[70px] px-6 shadow-lg '>
@@ -18,6 +24,21 @@ const TopBar = ({ toggleSidebar }) => {
             </div>
 
             <div className='relative flex items-center gap-4'>
+                {decoded.user_role === 'Seller' ? (
+                    <div>
+                        <a
+                            type='button'
+                            href={`https://wa.me/6287833380742?text=Halo,%20saya%20${user?.nama}%20Ingin%20Bertanya?`}
+                            className='inline-flex w-full py-2 mx-1 mt-2 font-semibold text-white bg-green-500 rounded-lg'
+                            target='_blank'>
+                            <FaWhatsapp size={24} className='mx-2' />
+                            Hubungi Admin
+                        </a>
+                    </div>
+                ) : (
+                    <></>
+                )}
+
                 <div className='flex items-center gap-3'>
                     <p className='font-bold tracking-widest'>{user?.nama}</p>
                     <img
