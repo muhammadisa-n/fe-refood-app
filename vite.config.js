010b5816a605ca43d9ee6e6213c972af57548270
muhammadisa-n <muhammadisa226@gmail.com>
2025-07-17 14:26:36 +0700

    fix routing

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'node:url'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/refood-app/',
+    base: '/refood/',
     resolve: {
         alias: [
             {
